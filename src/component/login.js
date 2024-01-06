2f442f23c6b3199b28dc4d5e610d5d5850840182
KHARANSHUWANARE <kharanshuw@gmail.com>
2024-01-06 15:00:08 +0530

    #

--- src/component/login.js
+++ src/component/login.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/login1.css';
 import email from './images/email.png';
 import user from './images/user.jpg';
 import password from './images/password.jpg';
+import { useSearchParams } from 'react-router-dom';
 const Login = () => {
+  const [action,setaction]=useState("login");
   return (
     <div className='container'>
     <div className='header'>
-    <div className='text'>sign up</div>
+    <div className='text'>{action}</div>
     <div className='underline'> </div>
     </div>
       <div className='inputs'>
@@ -26,8 +28,8 @@ const Login = () => {
       </div>
       <div className='forget-password'>lost password?<span>click hear?</span></div>
       <div className='submit-container'>
-        <div className='submit'>sign up</div>
-        <div className='submit'>login</div>
+        <div className={action==="login"?"submit gray":"submit"} onClick={()=>{setaction("login")}}></div>
+        <div className={action==="sign up"?"submit gray ":"submit" } onClick={()=>{setaction("sign up")}}>login</div>
       </div>
     </div>
   )
